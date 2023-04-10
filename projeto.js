e09528849f732a992392c4e1100a65224ef1961d
Ricardo Buri <ricardogomess921@gmail.com>
2023-04-10 11:27:37 -0300

    2.0 versão

--- projeto.js
+++ projeto.js
@@ -1,36 +1,10 @@
 const meuPrimeiroApp = Vue.createApp({
 
-    data() { //criando variaveis
-        return {
-            msgInicial: 'Bem-vindo ao VUE.JS', 
-            verMsg: false,
-            corMsg: false,
-            contador: 0,
-            cores: [
-                { ptbr: 'AZUL' },
-                { ptbr: 'VERDE' },
-                { ptbr: 'VERMELHO' }
-            ],
-            msgText: "Para nada!"     //computed  
-        }
-    },
-
-
-    // diferença de "computed" para "methods"
-    //methods vai deixar um valor relacionado diretamente ao alert
-    //computed tem que ligar a variável com o "this."
-    methods: {
-        avisoSistema(msgText) {
-            alert(this.msgText)  
+    data(){
+        return{
+            textoDinamico: null,
+            corTexto: "Black"
         }
     }
 })
-
-// criação do template para reutilizar os elementos criados
-meuPrimeiroApp.component('botao-inutil', {
-    template: `
-    <button>
-    Utilidade deste app
-    </button> `
-})
-const mountedApp = meuPrimeiroApp.mount('#divPrincipal')
+const mountedApp = meuPrimeiroApp.mount('#formPrincipal')
